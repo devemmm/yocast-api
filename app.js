d9cb6f4f22938dbd29bb9be4e30d0658ca3e24d9
devemmm <emmanuel@gennxttechsol.com>
2022-11-24 21:14:31 +0200

    adding index form

--- app.js
+++ app.js
@@ -7,23 +7,34 @@ const cors = require('cors')
 const swaggerUI = require('swagger-ui-express')
 const YAML = require('yamljs')
 const swaggerJsDocs = YAML.load('./api.yaml')
+const hbs = require('hbs')
+const favicon = require('serve-favicon');
 
 const adminRouter = require('./src/routes/adminRoutes')
 const userRouter = require('./src/routes/userRoutes')
+const publicRoute = require('./src/routes/publicRoutes')
 
 const port = process.env.PORT
 const app = express()
 
 const publicDirectoryPath = path.join(__dirname, './public')
+const viewsPath = path.join(__dirname, './template/views')
+const partialPath = path.join(__dirname, './template/partials')
 
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 app.use(cors())
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 app.use(express.static(publicDirectoryPath))
+
+hbs.registerPartials(partialPath)
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDocs))
+app.use(publicRoute)
 app.use(adminRouter)
 app.use(userRouter)
 
-app.listen(port, ()=>console.log(`Server is running on port ${port}`))
+module.exports = app;
